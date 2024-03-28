7bca494fd46c0dbcb74f00c809c2ee82cc25cc24
Open Risk <openrisk@outlook.com>
2024-03-28 16:31:15 +0100

    Fix missing header

--- exercises/step5/tensor_casting.cpp
+++ exercises/step5/tensor_casting.cpp
@@ -1,8 +1,9 @@
-// (c) 2023  Open Risk (https://www.openriskmanagement.com)
+// (c) 2023  - 2024 Open Risk (https://www.openriskmanagement.com)
 // Example Script for the Open Risk Academy Course DAT31071
 // Tensor Calculations with the Eigen C++ Library
 // https://www.openriskacademy.com/course/view.php?id=71#include <catch2/catch.hpp>
 
+#include <catch2/catch.hpp>
 #include "eigen-3.4.0/Eigen/Dense"
 #include "eigen-3.4.0/unsupported/Eigen/CXX11/Tensor"
 #include <iostream>
